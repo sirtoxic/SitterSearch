a3c4e2675379121b6e6584a786cf727983a6013a
Lee Russell <leerussell1991@gmail.com>
2017-04-25 00:51:19 +1000

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+      <div class="col-md-12">
+          <div class="col-md-4">
+            <div class="panel panel-default">
+              <div class="panel-body">
+                <img src="img/placeholder2.png" style="max-width: 100%;">
+                <p align="center">Baby Sitting</p>
+              </div>
+            </div>
+          </div>
+
+            <div class="col-md-4">
+              <div class="panel panel-default">
+                <div class="panel-body">
+                  <img src="img/placeholder1.jpeg" style="max-width: 100%;">
+                  <p align="center">Playdates</p>
+                </div>
+              </div>
+            </div>
+
+              <div class="col-md-4">
+                <div class="panel panel-default">
+                  <div class="panel-body">
+                    <img src="img/placeholder3.jpeg" style="max-width: 100%;">
+                    <p align="center">Sitting Jobs</p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-12">
+                <div class="panel panel-default">
+                  <div class="panel-body">
+                    <p align="center">Success Storys/ Reviews<br><br><br><br><br><br><br><br><br><br><br><br></p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-12">
+                <div class="panel panel-default">
+                  <div class="panel-body">
+                    <p align="center">About Us<br><br><br><br><br><br><br><br><br><br><br><br></p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-12">
+                <div class="panel panel-default">
+                  <div class="panel-body">
+                    <p align="center">Contact Us<br><br><br><br><br><br><br><br><br><br><br><br></p>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+    </div>
+</div>
+@endsection
